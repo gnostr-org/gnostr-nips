dbe2a8327b2a3fa40727ca7348fb1b7ba68250cd
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-16 09:24:37 -0400

    Cargo.toml:src/main.rs:add include_dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path::Path;
+use include_dir::{include_dir, Dir};
+static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR");
 fn main() {
-    println!("Hello, world!");
+    print_entries();
+}
+//#[cfg(feature = "glob")]
+fn print_entries() -> () {
+    let glob = "**/*.md";
+    for entry in PROJECT_DIR.find(glob).unwrap() {
+        println!("Found {}", entry.path().display());
+    }
 }
