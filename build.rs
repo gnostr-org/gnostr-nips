4421cbcb7f268433f0e514a4ca3c976618510248
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-17 08:49:49 -0400

    build.rs:git fetch/rebase upstream/master

---
+++ build.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    Command::new("git")
+        .args(&[
+            "remote",
+            "add",
+            "gnostr-org",
+            "git@github.com:gnostr-org/nips.git",
+        ])
+        .status()
+        .unwrap();
+    Command::new("git")
+        .args(&[
+            "remote",
+            "add",
+            "upstream",
+            "git@github.com:nostr-protocol/nips.git",
+        ])
+        .status()
+        .unwrap();
+    Command::new("git")
+        .args(&["fetch", "--all"])
+        .status()
+        .unwrap();
+    Command::new("git")
+        .args(&["fetch", "--all", "--tags"])
+        .status()
+        .unwrap();
+    Command::new("git")
+        .args(&["rebase", "upstream/master"])
+        .status()
+        .unwrap();
+}
