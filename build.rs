cba2bc8227a5bfdfd220c403bf42641e251d809f
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-18 19:33:24 -0400

    build.rs:md to html

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
-use std::env;
-//use std::path::Path;
 use std::process::Command;
+use std::{env, fs};
 
-fn main() {
+use include_dir::{include_dir, Dir};
+//use std::path::Path;
+use markdown::to_html;
+
+static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR");
+
+fn main() -> std::io::Result<()> {
     let _out_dir = env::var("OUT_DIR").unwrap();
 
     Command::new("git")
@@ -35,4 +40,25 @@ fn main() {
         .args(&["rebase", "upstream/master"])
         .status()
         .unwrap();
+
+    //pandoc README.md | sed 's/.md/.html/g'  > readme.html
+
+    let mut count: u8 = 0;
+    let glob = "**/*.md";
+    let mut nip_vec = Vec::<String>::new(); // Create an empty Vec
+
+    for entry in PROJECT_DIR.find(glob).unwrap() {
+        count = count + 1;
+        //println!("count={}", count);
+        //println!("{}", entry.path().display());
+        nip_vec.push((entry.path().display()).to_string().replace(".md", ".html"));
+        //nip_vec.push("md content".to_string());
+        let mut md_content = PROJECT_DIR.get_file(entry.path()).unwrap();
+        let content = md_content.contents_utf8().unwrap();
+        //println!("\n{}", content);
+        println!("{}", markdown::to_html(content));
+        let content_html = markdown::to_html(content);
+        fs::write(format!("{}.html", count), content_html)?;
+    }
+    Ok(())
 }
