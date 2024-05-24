6d64a7cb796dee26902662a0a101188ae9138d61
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-24 09:18:10 -0400

    webview-examples:use nips

--- webview-examples/examples/fullscreen.rs
+++ webview-examples/examples/fullscreen.rs
@@ -1,9 +1,9 @@
-extern crate web_view;
+extern crate nips;
 
-use web_view::*;
+use nips::*;
 
 fn main() {
-    web_view::builder()
+    nips::builder()
         .title("Fullscreen example")
         .content(Content::Html(HTML))
         .size(800, 100)
--- webview-examples/examples/timer.rs
+++ webview-examples/examples/timer.rs
@@ -1,19 +1,19 @@
 //#![windows_subsystem = "windows"]
 
-extern crate web_view;
+extern crate nips;
 
 use std::{
     sync::{Arc, Mutex},
     thread,
     time::Duration,
 };
-use web_view::*;
+use nips::*;
 
 fn main() {
     let counter = Arc::new(Mutex::new(0));
 
     let counter_inner = counter.clone();
-    let webview = web_view::builder()
+    let webview = nips::builder()
         .title("Timer example")
         .content(Content::Html(HTML))
         .size(800, 600)
