6ebc091ef30cb6c2f1ae3dc048702efc1230e5af
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-05-24 12:14:21 -0400

    Cargo.toml:src/main.rs:webview-sys:apply cargo +nightly fmt/sort

--- webview-sys/lib.rs
+++ webview-sys/lib.rs
@@ -1,7 +1,7 @@
 //! Raw FFI bindings to [webview].
 //!
-//! To use a custom version of webview, define an environment variable `WEBVIEW_DIR` with the path
-//! to its source directory.
+//! To use a custom version of webview, define an environment variable
+//! `WEBVIEW_DIR` with the path to its source directory.
 //!
 //! [webview]: https://github.com/zserge/webview
 
